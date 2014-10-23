c9da9ca5ebfe6fa8c14602503efce09f44722d07
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-10-24 01:33:10 +0200

    Problems 4 - 7 completed. Refactor later

--- problems4_7.m
+++ problems4_7.m
@@ -94,8 +94,8 @@ for i = 1:length(g)
 end
 variance = mean(vars);
 E_out_a = bias + variance;
-fprintf('Optoin(a)\th(x) = b\t%f\n', E_out_a);
-fprintf('Optoin(b)\th(x) = ax\t%f\n', E_out_b);
+fprintf('Option(a)\th(x) = b\t%f\n', E_out_a);
+fprintf('Option(b)\th(x) = ax\t%f\n', E_out_b);
 
 %% Option c
 %% use linear regression to find min SSE hypothesis on two random points
@@ -135,5 +135,83 @@ for i = 1:length(g)
 end
 variance = mean(vars);
 E_out_c = bias + variance;
-fprintf('Optoin(a)\th(x) = ax + b\t%f\n', E_out_c);
+fprintf('Option(c)\th(x) = ax + b\t%f\n', E_out_c);
 
+%% option d
+%% use linear regression to find min SSE hypothesis on two random points
+a = -1; b = 1; N = 2; num_iters = 1000;
+g = zeros(num_iters, 1);
+X_TRAIN = [];
+F_TRAIN = [];
+
+for i = 1:num_iters
+    % pick two random training points
+    x_train = a + (b - a) .* rand(N, 1);
+    % compute corresponding y axis value according to the target function
+    % f(x) = sin(pi * x)
+    f_train = sin(pi * x_train);
+    x_train = x_train.^2;
+    % use normal equation to get the min SSE hypothesis (g)
+    g(i) = pinv(x_train' * x_train) * x_train' * f_train;
+
+    %== Use the following to calcualte the bias and variance ==%
+    % combine all x_train points in 1 vector
+    X_TRAIN = [X_TRAIN x_train'];
+    % combine all f_train points in 1 vector
+    F_TRAIN = [F_TRAIN f_train'];
+end
+
+g_bar = mean(g);
+% bias = (g_bar(x) - f(x)).^2
+bias = mean((g_bar * X_TRAIN - F_TRAIN).^2);
+
+% variance = g_each_data_set(x) - g_bar(x)).^2
+vars = zeros(length(g), 1);
+for i = 1:length(g)
+    vars(i) = mean((g(i) * X_TRAIN - g_bar * X_TRAIN).^2);
+end
+variance = mean(vars);
+E_out_d = bias + variance;
+fprintf('Option(d)\th(x) = ax^2\t%f\n', E_out_d);
+
+%% option e
+%% use linear regression to find min SSE hypothesis on two random points
+a = -1; b = 1; N = 2; num_iters = 1000;
+g = zeros(num_iters, 2);    % now two weights incl. the intercept term
+X_TRAIN = [];
+F_TRAIN = [];
+
+for i = 1:num_iters
+    % pick two random training points
+    x_train = a + (b - a) .* rand(N, 1);
+    % compute corresponding y axis value according to the target function
+    % f(x) = sin(pi * x)
+    f_train = sin(pi * x_train);
+
+    % first add intercept term to x_train
+    x_train = [ones(size(x_train, 1), 1) x_train];
+    x_train = x_train.^2;
+    % use normal equation to get the min SSE hypothesis (g)
+    g(i, :) = pinv(x_train' * x_train) * x_train' * f_train;
+
+    %== Use the following to calcualte the bias and variance ==%
+    % combine all x_train points in 1 vector
+    X_TRAIN = [X_TRAIN x_train'];
+    % combine all f_train points in 1 vector
+    F_TRAIN = [F_TRAIN f_train'];
+end
+
+g_bar = mean(g);
+% bias = (g_bar(x) - f(x)).^2
+bias = mean((g_bar * X_TRAIN - F_TRAIN).^2);
+
+% variance = g_each_data_set(x) - g_bar(x)).^2
+vars = zeros(length(g), 1);
+for i = 1:length(g)
+    vars(i) = mean((g(i, :) * X_TRAIN - g_bar * X_TRAIN).^2);
+end
+variance = mean(vars);
+E_out_e = bias + variance;
+fprintf('Option(e)\th(x) = ax^2 + b\t%f\n', E_out_e);
+
+%%
